a6dd209e870e0dcf321687336388ad6b4dfe49c1
Jeff Walker <walker.jeff.d@gmail.com>
2014-09-10 15:03:20 -0400

    add aggregate_maurer_mon to convert monthly to annual timeseries

--- R/get_maurer.R
+++ R/get_maurer.R
@@ -11,7 +11,24 @@ get_maurer_mon <- function(lat, lon) {
   distances <- sqrt((maurer$grid$LAT-lat)^2 + (maurer$grid$LON-lon)^2)
   pos <- c(maurer$grid[which.min(distances),])
   maurer_ts <- subset(maurer$data, LAT==pos$LAT & LON==pos$LON)
-  return(list(location=list(latitude=lat, longitude=lon),
-              nearest=list(latitude=pos$LAT, longitude=pos$LON),
-              data=maurer_ts))
+  return(maurer_ts)
+}
+
+#' Aggregate monthly timeseries from maurer to annual timesteps
+#'
+#' @param x data frame containing monthly maurer timeseries
+#' @export
+#' @examples
+#' aggregate_maurer_mon(get_maurer_mon(42, -71))
+#'
+aggregate_maurer_mon <- function(x) {
+  require(dplyr)
+  x <- dplyr::mutate(x, N_DAY=lubridate::days_in_month(DATE))
+  grp <- dplyr::group_by(x, YEAR, LAT, LON)
+  x.yr <- dplyr::summarise(grp,
+                           PRCP=sum(PRCP),
+                           TMAX=sum(TMAX*N_DAY)/sum(N_DAY),
+                           TMIN=sum(TMIN*N_DAY)/sum(N_DAY),
+                           WIND=sum(WIND*N_DAY)/sum(N_DAY))
+  return(x.yr)
 }
